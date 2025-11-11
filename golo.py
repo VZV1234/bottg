17827a59c4727c6b60adb31b5f3ee9e63a8ad213
VZV1234 <g53227927@gmail.com>
2025-11-11 15:29:16 +0300

    Update golo.py

--- golo.py
+++ golo.py
@@ -10,11 +10,14 @@ CREATOR_ID = '5067637273'  # замените на ваш реальный id
 
 bot = telebot.TeleBot(BOTTOKEN)
 
+# Хранение состояния каждого пользователя (может отправлять отзыв или нет)
+can_send_feedback = {}  # По умолчанию False
+
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
+    can_send_feedback[message.chat.id] = True  # разрешаем отправлять отзыв
     # Приветствие пользователя
-    # Создаем inline-клавиатуру
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton(text="РАСПИСАНИЕ", url="https://p11505.edu35.ru/gmraspisanie/izmeneniya")
     btn2 = types.InlineKeyboardButton(text="СДО", url="https://sdo.p11505.edu35.ru")
@@ -24,17 +27,17 @@ def start(message):
     markup.row(btn3, btn2)
     markup.row(btn4)
 
-    # Отправляем сообщение с inline-клавиатурой
     bot.send_message(message.chat.id, f'''
 Привет, {message.from_user.first_name}! 
 Это бот-помощник для студентов Череповецкого химико-технологического колледжа 
 Приятного использования!
+demo1.0
 ''', reply_markup=markup)
 
 # Обработчик нажатия на кнопку "Обратная связь"
 @bot.callback_query_handler(func=lambda call: call.data == 'feedback')
 def feedback_button_pressed(call):
-    # Просим пользователя оставить отзыв
+    can_send_feedback[call.message.chat.id] = True  # разрешаем отправлять отзыв
     bot.send_message(call.message.chat.id, '''
 Отправьте ваше пожелание или предложение в виде обычного сообщения.
 Оно будет передано создателям бота.
@@ -43,11 +46,18 @@ def feedback_button_pressed(call):
 # Обработчик отзыва
 @bot.message_handler(func=lambda m: True)
 def collect_feedback(message):
-    # Передаем отзыв создателю бота
-    bot.send_message(CREATOR_ID, f'''Новый отзыв от пользователя {message.from_user.username}: 
+    if can_send_feedback.get(message.chat.id, False):
+        # Если пользователь может отправить отзыв
+        can_send_feedback[message.chat.id] = False  # запрещаем повторять отправку
+        
+        # Передаем отзыв создателю бота
+        bot.send_message(CREATOR_ID, f'''Новый отзыв от пользователя {message.from_user.username}: 
 {message.text}''')
-    # Сообщаем пользователю, что отзыв отправлен
-    bot.send_message(message.chat.id, 'Спасибо за ваш отзыв!')
+        # Сообщаем пользователю, что отзыв отправлен
+        bot.send_message(message.chat.id, 'Отправлено')
+    else:
+        # Если нельзя отправить отзыв, просто проигнорируем сообщение
+        pass
 
 # Запуск бота
 bot.polling(none_stop=True)
